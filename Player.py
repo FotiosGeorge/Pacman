64a08d2bd4a3d991f7a894b66dc2eab3b1c8ef28
FotiosGeorge <53009578+FotiosGeorge@users.noreply.github.com>
2019-09-18 19:03:46 +0100

    Add files via upload

--- Player.py
+++ Player.py
@@ -4,3 +4,14 @@ import pygame
 class Player(object):
     def __init__(self, board):
         self.board = board
+        self.x = 630 - self.board.offset_width
+        self.y = 432 - self.board.offset_height
+        self.starting_pos = [self.x, self.y]
+        self.user_events()
+
+    def user_events(self):
+        self.draw_start()
+
+    def draw_start(self):
+        pygame.draw.circle(self.board.window, (255, 255, 0), (self.starting_pos[0], self.starting_pos[1]), 8)
+        pygame.display.update()
