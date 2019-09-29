941970a682b87112041f334334bae3921f01b7b9
FotiosGeorge <53009578+FotiosGeorge@users.noreply.github.com>
2019-09-29 17:22:26 +0100

    Add files via upload
    I added to the update function, to remove the dots from the screen once pacman is in the same location as a dot(eats one).
    This caused difficulty at first as I was passing by reference self.dots = self.free_cells. I fixed this by appending all the locations to self.dots in the game file. This mean't that I was passing by reference anymore and I was not removing from the self.free_cells list and just the self.dots list.

--- Player.py
+++ Player.py
@@ -6,7 +6,7 @@ class Player(object):
         self.board = board
         self.x = 607
         self.y = 420
-        self.pos = (self.x, self.y)
+        self.pos = [(self.x, self.y)]
 
     def movement(self, x, y):
         self.x += x
@@ -14,8 +14,12 @@ class Player(object):
         self.pos = [self.x, self.y]
 
     def draw(self):
-        pygame.draw.circle(self.board.window, (255, 255, 0), (self.pos[0], self.pos[1]), 8)
+        pygame.draw.circle(self.board.window, (255, 255, 0), (self.x, self.y), 8)
 
     def update(self):
+        for tup2 in self.board.dots:
+            if (tup2[0] == self.x) and (tup2[1] == self.y):
+                self.board.dots.remove(tup2)
+                break
         pygame.display.update()
 
