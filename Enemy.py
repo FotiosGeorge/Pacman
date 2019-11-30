67ca57f8b9efc815dc2f613f5b423683cae22720
FotiosGeorge <53009578+FotiosGeorge@users.noreply.github.com>
2019-11-30 13:32:58 +0000

    Add files via upload

--- Enemy.py
+++ Enemy.py
@@ -8,6 +8,7 @@ class Enemy:
         self.y = 276
         self.direction = direction
         self.colour = colour
+        self.intersections = []
         self.matrix = []
         self.matrix_equivalent = {}
 
@@ -35,28 +36,42 @@ class Enemy:
         "---------------------------------------------------------------------"
 
     def create_matrix(self):
-        index = 0
+
         for value in self.board.free_pos:
+            x1 = (value[0] + 1, value[1])
+            x2 = (value[0] - 1, value[1])
+            y1 = (value[0], value[1] + 1)
+            y2 = (value[0], value[1] - 1)
+            if x1 in self.board.free_pos or x2 in self.board.free_pos:
+                if y1 in self.board.free_pos or y2 in self.board.free_pos:
+                    if value not in self.intersections:
+                        self.intersections.append((value[0], value[1]))
+
+        index = 0
+        for value in self.intersections:
             self.matrix_equivalent[value] = index
             index += 1
-        for x in range(0, len(self.board.free_pos)):
+        for x in range(0, len(self.intersections)):
             self.matrix.append([])
-            for y in range(0, len(self.board.free_pos)):
+            for y in range(0, len(self.intersections)):
                 self.matrix[x].append(float("inf"))
-        for index, node in enumerate(self.board.free_pos):
-            if ((node[0] + 1), (node[1])) in self.board.free_pos:
-                index2 = self.board.free_pos.index(((node[0] + 1), (node[1])))
-                self.matrix[index][index2] = 1
-            if ((node[0] - 1), (node[1])) in self.board.free_pos:
-                index2 = self.board.free_pos.index(((node[0] - 1), (node[1])))
-                self.matrix[index][index2] = 1
-            if ((node[0]), (node[1] + 1)) in self.board.free_pos:
-                index2 = self.board.free_pos.index(((node[0]), (node[1] + 1)))
-                self.matrix[index][index2] = 1
-            if ((node[0]), (node[1] - 1)) in self.board.free_pos:
-                index2 = self.board.free_pos.index(((node[0]), (node[1] - 1)))
-                self.matrix[index][index2] = 1
+
+        for index, value in enumerate(self.intersections):
+            found = False
+            for index2, value2 in enumerate(self.intersections):
+                if value != value2 and found is False:
+                    if value[0] == value2[0]:
+                        found = True
+                        weight = abs(value2[1] - value[1])
+                        self.matrix[index][index2] = weight
+                    if value[1] == value2[1]:
+                        found = True
+                        weight = abs(value2[0] - value[0])
+                        self.matrix[index][index2] = weight
         print(self.matrix)
+        print(self.matrix_equivalent)
+
+
 
     def neighbours_location(self):
         None
